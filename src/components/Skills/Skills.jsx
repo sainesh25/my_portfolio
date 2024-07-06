129f25b8e2fc5e718ad0a4d7283a5d71b917c059
SaineshPatil <saineshpatil@kenmarkitan.com>
2024-07-06 15:19:18 +0530

    updated skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,32 +2,43 @@ import React from 'react'
 
 import './Skills.css'
 export default function Skills() {
+    const frontendSkills = ["JavaScript", "React.js", "Bootstrap", "Next.js", "Tailwind CSS",  "HTML5", "CSS3", "jQuery"];
+
+    const backendSkills = ["Node.js", "Express.js"];
+
+    const tools = ["Git", "Fork", "GitLab", "GitHub"];
+
     return (
         <>
-        <div className="skills-section" id='skillId'>
-            <h1 className='skills-head'>What I am Good at ?</h1>
-            <div className="skills-main-wrapper">
-                <h2 className='frontend-head'>Frontend Technologies</h2>
-                <div className="frontend-skills">
-                    <div className="skills-card">HTML5</div>
-                    <div className="skills-card">CSS3</div>
-                    <div className="skills-card">JavaScript</div>
-                    <div className="skills-card">ReactJs</div>
-                    <div className="skills-card">Bootstrap</div>
-                    <div className="skills-card">jQuery</div>
-                    <div className="skills-card">Tailwind CSS</div>
-                </div>
+            <div className="skills-section" id='skillId'>
+                <h1 className='skills-head'>What I am Good at ?</h1>
+                <div className="skills-main-wrapper">
+                    <h2 className='frontend-head'>Frontend Technologies</h2>
+                    <div className="frontend-skills">
+                        {
+                            frontendSkills.map((item, index) => <div key={index} className="skills-card">{item}</div>)
+                        }
+                    </div>
                     <h2 className='backend-head'>Backend Technologies</h2>
-                <div className="backend-skills">
-                    <div className="skills-card backend-skills-card">NodeJs</div>
-                    <div className="skills-card backend-skills-card">ExpressJs</div>
-                </div>
+                    <div className="backend-skills">
+                        {
+                            backendSkills.map((item, index) => <div key={index} className="skills-card backend-skills-card">{item}</div>)
+                        }
+
+                    </div>
                     <h2 className="database-head frontend-head">Database Technologies</h2>
-                <div className="database-skills">
-                    <div className="skills-card backend-skills-card">MongoDB</div>
+                    <div className="database-skills">
+                        <div className="skills-card backend-skills-card">MongoDB</div>
+                    </div>
+
+                    <h2 className="database-head frontend-head">Tools</h2>
+                    <div className="database-skills">
+                        {
+                            tools.map((item, index) => <div key={index} className="skills-card backend-skills-card">{item}</div>)
+                        }
+                    </div>
                 </div>
-            </div>      
-        </div>
+            </div>
         </>
     )
 }
