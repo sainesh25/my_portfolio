c4693646bb0ea638f813064a9b7718aea2ffb3eb
sainesh25 <patilsainesh25@gmail.com>
2025-01-08 15:22:01 +0530

    meta keywords added

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ const geistMono = localFont({
 export const metadata = {
   title: "Sainesh Patil",
   description: "Sainesh Patil | Full-Stack Developer specializing in MERN stack. Crafting seamless, responsive web solutions from front-end to back-end.",
+  keywords: 'Sainesh, Patil, Sainesh Patil, Full-Stack Developer, MERN Stack Developer, Frontend Development, Backend Development, Responsive Web Solutions, Web Developer Portfolio, React.js Developer, Node.js Developer, Next.js Developer, SEO, Tailwind CSS, JavaScript Expert, Modern Web Design, Scalable Web Applications, Full-Stack Solutions, Innovative Web Development',
   openGraph:{
     title: 'Sainesh Patil',
     description: 'Sainesh Patil | Full-Stack Developer specializing in MERN stack. Crafting seamless, responsive web solutions from front-end to back-end.',
